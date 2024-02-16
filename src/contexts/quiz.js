38e82a50d3e0f6b686fd9b6858f2b928bb2f72dd
Levon Aslanyan <aslanyan2012@mail.ru>
2024-02-16 11:10:37 +0400

    finish

--- src/contexts/quiz.js
+++ src/contexts/quiz.js
@@ -1,43 +1,41 @@
 import { createContext, useReducer } from "react";
-import questions from "../data";
-import { shuffleAnswers } from "../helpers";
+import { shuffleAnswers, normalizeQuestions } from "../helpers";
 
 const initialState = {
   currentQuestionIndex: 0,
-  questions: questions, // if the name is the same you can write one
+  questions: [],
   showResults: false,
-  answers: shuffleAnswers(questions[0]),
+  answers: [],
   currentAnswer: "",
-  correctAnswerCount: 0,
+  correctAnswersCount: 0,
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "SELECT_ANSWER": {
-      const correctAnswerCount =
+      const correctAnswersCount =
         action.payload ===
         state.questions[state.currentQuestionIndex].correctAnswer
-          ? state.correctAnswerCount + 1
-          : state.correctAnswerCount;
-
+          ? state.correctAnswersCount + 1
+          : state.correctAnswersCount;
       return {
         ...state,
         currentAnswer: action.payload,
-        correctAnswerCount,
+        correctAnswersCount,
       };
     }
     case "NEXT_QUESTION": {
-      const showResults = state.currentQuestionIndex === questions.length - 1;
-      const currentQuestionNumber = showResults
+      const showResults =
+        state.currentQuestionIndex === state.questions.length - 1;
+      const currentQuestionIndex = showResults
         ? state.currentQuestionIndex
         : state.currentQuestionIndex + 1;
       const answers = showResults
         ? []
-        : shuffleAnswers(state.questions[currentQuestionNumber]);
-
+        : shuffleAnswers(state.questions[currentQuestionIndex]);
       return {
         ...state,
-        currentQuestionIndex: currentQuestionNumber,
+        currentQuestionIndex,
         showResults,
         answers,
         currentAnswer: "",
@@ -46,15 +44,23 @@ const reducer = (state, action) => {
     case "RESTART": {
       return initialState;
     }
+    case "LOADED_QUESTIONS": {
+      const normalizedQuestions = normalizeQuestions(action.payload);
+      return {
+        ...state,
+        questions: normalizedQuestions,
+        answers: shuffleAnswers(normalizedQuestions[0]),
+      };
+    }
     default: {
       return state;
     }
   }
 };
 
-export const QuizContex = createContext();
+export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
   const value = useReducer(reducer, initialState);
-  return <QuizContex.Provider value={value}>{children}</QuizContex.Provider>;
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
 import Quiz from "./components/Quiz";
-import { QuizProvider } from "./contex/quiz";
+import { QuizProvider } from "./contexts/quiz";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <QuizProvider>
       <Quiz />
     </QuizProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
